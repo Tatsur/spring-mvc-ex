5ca0ccae26a2c345f2af1701777a6afe75dd3f13
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-08-13 18:11:57 +0700

    Initial commit

---
+++ src/test/java/com/ttsr/springmvcex/SpringMvcExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttsr.springmvcex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
